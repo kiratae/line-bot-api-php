1aedc0793b166eb2f32e48ec310e253af2391bec
kiratae <31317587+kiratae@users.noreply.github.com>
2020-03-25 18:23:54 +0700

    Update line.php

--- line.php
+++ line.php
@@ -10,73 +10,100 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 if ( sizeof($request_array['events']) > 0 )
 {
 
- foreach ($request_array['events'] as $event)
- {
-  $reply_message = '';
-  $reply_token = $event['replyToken'];
+foreach ($request_array['events'] as $event)
+{
+$reply_message = '';
+$reply_token = $event['replyToken'];
 
-  if ( $event['type'] == 'message' ) 
-  {
-   
-   if( $event['message']['type'] == 'text' )
-   {
-		$text = $event['message']['text'];
-		
-		if(($text == "อยากทราบยอด COVID-19 ครับ")||($text == "ยอด COVID-19")||($text == "อยากทราบยอด COVID-19 ครับ")){
-			$cumulative = 955;
-			$death = 4;
-			$fine = 50;
-			$reply_message = '"รายงานสถานการณ์ ยอดผู้ติดเชื้อไวรัสโคโรนา 2019 (COVID-19) ในประเทศไทย" \n
-			ผู้ป่วยสะสม	จำนวน '.$cumulative.' ราย \n
-			ผู้เสียชีวิต	จำนวน '.$death.' ราย \n
-			รักษาหาย	จำนวน '.$fine.' ราย \n
-			ผู้รายงานข้อมูล: 59160180 นายธนภร เกลี้ยกล่อม';
-		}
-		else if(($text== "ข้อมูลส่วนตัวของผู้พัฒนาระบบ")||($text== "ข้อมูลส่วนตัว")||($text== "ข้อมูลผู้พัฒนา")||($text== "ข้อมูลผู้พัฒนาระบบ")){
-			$reply_message = 'ชื่อนายธนภร เกลี้ยกล่อม อายุ 22ปี น้ำหนัก 68kg. สูง 170cm. ขนาดรองเท้าเบอร์ 8 ใช้หน่วย US';
-		}
-		else
-		{
-			$reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
-    		}
-   
-   }
-   else
-    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
-  
-  }
-  else
-   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- 
-  if( strlen($reply_message) > 0 )
-  {
-   //$reply_message = iconv("tis-620","utf-8",$reply_message);
-   $data = [
-    'replyToken' => $reply_token,
-    'messages' => [['type' => 'text', 'text' => $reply_message]]
-   ];
-   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+if ( $event['type'] == 'message' ) 
+{
 
-   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-   echo "Result: ".$send_result."\r\n";
-  }
- }
+if( $event['message']['type'] == 'text' )
+{
+	$text = $event['message']['text'];
+
+	if(($text == "อยากทราบยอด COVID-19 ครับ")||($text == "ยอด COVID-19")||($text == "อยากทราบยอด COVID-19 ครับ"||($text == "COVID-19")||($text == "โควิด"))){
+		$cumulative = 955;
+		$death = 4;
+		$fine = 50;
+		$reply_message = '"รายงานสถานการณ์ ยอดผู้ติดเชื้อไวรัสโคโรนา 2019 (COVID-19) ในประเทศไทย"
+ผู้ป่วยสะสม	จำนวน '.$cumulative.' ราย
+ผู้เสียชีวิต	จำนวน '.$death.' ราย
+รักษาหาย	จำนวน '.$fine.' ราย
+ผู้รายงานข้อมูล: 59160180 นายธนภร เกลี้ยกล่อม';
+	}
+	else if(($text== "ข้อมูลส่วนตัวของผู้พัฒนาระบบ")||($text== "ข้อมูลส่วนตัว")||($text== "ข้อมูลผู้พัฒนา")||($text== "ข้อมูลผู้พัฒนาระบบ")){
+		$reply_message = 'ชื่อนายธนภร เกลี้ยกล่อม อายุ 22ปี น้ำหนัก 68kg. สูง 170cm. ขนาดรองเท้าเบอร์ 8 ใช้หน่วย US';
+	}
+	else
+	{
+		$reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+	}
+
+}
+else
+$reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+
+}
+else
+	$reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+
+	if( strlen($reply_message) > 0 )
+	{
+		//$reply_message = iconv("tis-620","utf-8",$reply_message);
+		$data = [
+		'replyToken' => $reply_token,
+		'messages' => [['type' => 'text', 'text' => $reply_message]]
+		];
+		$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+		$send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+		echo "Result: ".$send_result."\r\n";
+		}
+	}
 }
 
 echo "OK";
 
 function send_reply_message($url, $post_header, $post_body)
 {
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
 
- return $result;
+	return $result;
 }
 
+function getCovidData(){
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://opend.data.go.th/get-ckan/datastore_search?resource_id=93f74e67-6f76-4b25-8f5d-b485083100b6&limit=5";,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+		"api-key: YBDoHoTGjOzeCRXKd7jNoZBLNH7CDOaY"
+		)
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+		echo $response;
+	}
+}
 ?>
